06ad10af147a6ff2f133d32ee1b36d854a9c3d18
SoufianeAziz <azizsoufiane04@gmail.com>
2023-09-14 15:39:31 -0700

    good

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,7 +4,6 @@ def weight_average(my_list=[]):
         return 0.0
     weighted_sum = 0
     total_weight = 0
-
     for score, weight in my_list:
         weighted_sum += score * weight
         total_weight += weight
